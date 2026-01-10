a1bf12b8c4197f6faafa65fbae3d5e41e925a0cd
Developer <tanawattawillha@gmail.com>
2026-01-10 20:09:11 +0700

    update api path

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://api.map.meo.pp.ua/',
         changeOrigin: true,
       },
     },
